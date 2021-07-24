de62d9e9a0e03cc6b81686b0fe5eb8955912241a
Derrick Yam <keatkeat87@live.com>
2021-07-24 13:21:04 +0800

    key vault

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using AzureGetStarted.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace AzureGetStarted.Pages
 {
@@ -14,21 +15,29 @@ namespace AzureGetStarted.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _db;
+        private readonly IConfiguration _configuration;
 
         public IndexModel(
             ILogger<IndexModel> logger,
-            ApplicationDbContext Db
+            ApplicationDbContext Db,
+            IConfiguration configuration
         )
         {
             _logger = logger;
             _db = Db;
+            _configuration = configuration;
         }
+
+        public List<Product> Products { get; set; } = new();
+
+        public string SecretValue { get; set; } = "";
 
         public async Task OnGetAsync()
         {
-            var products = await _db.Products.ToListAsync();
+            Products = await _db.Products.ToListAsync();
             _db.Products.Add(new Product { Name = "Product1" });
             await _db.SaveChangesAsync();
+            SecretValue = _configuration["User:Password"];
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AzureGetStarted
 {
@@ -18,9 +17,28 @@ namespace AzureGetStarted
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var azureKeyVaultName = "JBReviews-KV3";
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        var secretClient = new SecretClient(
+                            new Uri($"https://{azureKeyVaultName}.vault.azure.net/"),
+                            new DefaultAzureCredential());
+                        config.AddAzureKeyVault(secretClient, new HyphenVaultSecretManager());
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    public class HyphenVaultSecretManager : KeyVaultSecretManager
+    {
+        public override string GetKey(KeyVaultSecret secret)
+        {
+            return secret.Name.Replace("-", ConfigurationPath.KeyDelimiter);
+        }
     }
 }
